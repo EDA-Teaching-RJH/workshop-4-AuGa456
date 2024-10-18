e98dd4759d252c43de22550dcff620733ca3b0c1
AuGa456 <jsl37@kent.ac.uk>
2024-10-18 10:15:24 +0000

    task 4

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,8 @@
+print("Hint: camelCaseLooksLikeThis ")
+variable = input("Please input a variable in camel case. ")
 
+for i in variable:
+    if i.isupper():
+        print("_", i.lower(), end="")
+    else:
+        print(i, end="")
--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,17 @@
-
+print("This machine accepts 50p, 20p, 10p and 5p coins. ")
+print("The drink costs £1. ")
+due1 = 100
+money1 = 0
+while money1 < due1:
+    money2 = int(input("Please insert coins one at a time. "))
+    if money2 != 50 and money2 != 20 and money2 != 10 and money2 != 5:
+        print("This machine does not accept that type of coin. ")
+    else:
+        money1 = money1 + money2
+        if money1 < due1:
+            print("You have inserted", money1, "\nYou need to insert", due1 - money1, "more. ")
+        elif money1 == due1:
+            print("You have payed for your drink, Enjoy. ")
+        else:
+            change = money1 - due1
+            print("You are owed", change, "in change. ")
